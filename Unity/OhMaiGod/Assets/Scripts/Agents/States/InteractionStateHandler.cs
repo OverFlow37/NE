3bf43236794e962891b94e53a3c3fd11c323fe34
권오준 <koj566@naver.com>
2025-05-11 19:08:52 +0900

    feat: 상호작용 진행과 주기 기능 구현

--- Unity/OhMaiGod/Assets/Scripts/Agents/States/InteractionStateHandler.cs
+++ Unity/OhMaiGod/Assets/Scripts/Agents/States/InteractionStateHandler.cs
@@ -9,6 +9,12 @@ namespace OhMAIGod.Agent
         {
             base.OnStateEnter(_controller);
 
+            // 상호작용 UI 시작
+            if (_controller.mAgentUI != null)
+            {
+                string interactionText = _controller.CurrentAction != null ? _controller.CurrentAction.Reason : "";
+                _controller.mAgentUI.StartInteractionUI(interactionText);
+            }
             // 상호작용 시작
             _controller.StartInteraction();
         }
@@ -22,6 +28,11 @@ namespace OhMAIGod.Agent
         public override void OnStateExit(AgentController _controller)
         {
             base.OnStateExit(_controller);
+            // 상호작용 UI 종료
+            if (_controller.mAgentUI != null)
+            {
+                _controller.mAgentUI.EndInteractionUI();
+            }
         }
 
         protected override string GetStateName()
